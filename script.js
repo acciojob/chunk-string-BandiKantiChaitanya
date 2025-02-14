ea05b63fa0fec4e4c0ac5b6b547aaa1f4d872fc4
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-14 16:49:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function stringChop(str, size) {
   // your code here
+	let result = [];
+    for (let i = 0; i < str.length; i += size) {
+        // Slice the string from index i to index i + size
+        result.push(str.slice(i, i + size));
+    }
+
+    return result;
 }
 
 // Do not change the code below
