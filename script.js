53b0284312404728b8b7d8b48ecdb168dece6b2a
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-14 17:01:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function stringChop(str, size) {
   // your code here
+	if (!str) {
+        return [];
+    }
 	let result = [];
     for (let i = 0; i < str.length; i += size) {
         // Slice the string from index i to index i + size
@@ -12,5 +15,5 @@ function stringChop(str, size) {
 // Do not change the code below
 const str = prompt("Enter String."); 
 const size = prompt("Enter Chunk Size.");
-size = parseInt(size);
+// size = parseInt(size); 
 alert(stringChop(str, size));
